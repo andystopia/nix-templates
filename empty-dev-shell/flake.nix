7f5b86091c23b7a3d9f71e6e25ff1579944f2df0
andystopia <91233633+andystopia@users.noreply.github.com>
2024-01-10 13:36:07 -0500

    feat(devshells): add a template for a basic devshell.
    This template exists to allow an easily fillable dev-shell
    that will allow the user to quickly define shell dependencies they
    want.

---
+++ empty-dev-shell/flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "FIXME: A basic starter devshell template";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "nixpkgs/nixos-23.11";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    # System types to support.
+    supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+  in {
+    # Provide some binary packages for selected system types.
+    packages = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+    });
+
+    # Add dependencies that are only needed for development
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          starship
+          # put other package below
+        ];
+
+        shellHook = "
+              eval \"$(starship init bash)\";
+            ";
+      };
+    });
+
+    formatter = forAllSystems (
+      system:
+        nixpkgsFor.${system}.alejandra
+    );
+  };
+}
